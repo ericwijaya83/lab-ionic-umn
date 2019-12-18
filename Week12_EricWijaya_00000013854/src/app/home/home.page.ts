babe6327dd41c4942124639eadbc926869a054b3
ericwijaya83 <ericwijaya83@gmail.com>
2019-12-18 22:11:31 +0700

    Pengumpulan Week 12

---
+++ Week12_EricWijaya_00000013854/src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { PlaceService } from './place.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  address = '';
+
+  constructor(private placeSvc: PlaceService,
+    private router: Router, 
+    private authSvc: AuthService) { }
+
+  ngOnInit(){
+    this.placeSvc.getAddress().subscribe(
+      currAddress => {
+        this.address = currAddress;
+      }
+    );
+  }
+
+  onLogout() {
+    this.authSvc.logout();
+    this.router.navigateByUrl('/auth');
+  }
+
+}
